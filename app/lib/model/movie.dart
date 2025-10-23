c4177bf9cebfa6ba70b31b956d42bb9308d2cc07
cleytonshelton <sheltonc8@nku.edu>
2025-10-23 09:06:07 -0400

    Added TV Shows
    Added a toggle button to the home screen that allows users to swtich between seeing movies and tv shows.

--- app/lib/model/movie.dart
+++ app/lib/model/movie.dart
@@ -15,11 +15,22 @@ class Movie {
       required this.posterPath});
 
   Movie.fromJson(Map<String, dynamic> parsedJson) {
-    this.id = parsedJson['id'] as int;
-    this.title = parsedJson['title'] as String? ?? '';
-    this.voteAverage = (parsedJson['vote_average'] as double?) ?? 0.0;
-    this.releaseDate = parsedJson['release_date'] as String? ?? '';
-    this.overview = parsedJson['overview'] as String? ?? '';
-    this.posterPath = parsedJson['poster_path'] as String? ?? '';
+    id= parsedJson['id'] ?? 0;
+
+    title = parsedJson['title'] ?? parsedJson['name'] ?? 'Untitled';
+
+    var vote = parsedJson['vote_average'];
+    if (vote is int) {
+      voteAverage = vote.toDouble();
+    } else if (vote is double) {
+      voteAverage = vote;
+    } else {
+      voteAverage = 0.0;
+    }
+
+    releaseDate = parsedJson['release_date'] ?? parsedJson['first_air_date'] ?? 'Unknown';
+
+    overview = parsedJson['overview'] ?? '';
+    posterPath = parsedJson['poster_path'] ?? '';
   }
 }
